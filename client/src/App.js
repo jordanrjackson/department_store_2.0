a9bd4af6c81f547537ec3c7517e5bb57ef138a18
Jordan Jackson <jordanrjackson0275@gmail.com>
2018-12-10 14:51:27 -0700

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment, } from 'react';
+import { Route, Switch, } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
+
+const App = () => (
+  <Fragment>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
+);
+
+export default App;
